dad5b2c1409baf7cd697e810f68bef154362f2ce
Adhi Christian <43411668+adhichristian07@users.noreply.github.com>
2018-10-04 02:19:52 +0700

    Add files via upload

---
+++ KonversiNominalIndonesiaApp/KonversiWindowsFormsApp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KonversiClassLibrary;
+
+namespace KonversiWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        Terbilang terbilang = new Terbilang();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(nominal.Text);
+            richTextBox1.Text = Convert.ToString(terbilang.TerbilangIndonesia(a));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            nominal.Text = "";
+            richTextBox1.Text = "";
+        }
+    }
+}
